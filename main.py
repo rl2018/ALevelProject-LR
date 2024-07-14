2eedfbeb0912e2cac0586fda2a9f328b60801dd3
rl2018 <152880652+rl2018@users.noreply.github.com>
2024-07-14 23:16:19 +0100

    image files, updated image class

--- main.py
+++ main.py
@@ -1,64 +1,96 @@
-import pygame, sys
+import pygame
+import sys
 
-#Setting up an environment to initialize pygame
+# Setting up an environment to initialize pygame
 mainClock = pygame.time.Clock()
 from pygame.locals import *
 pygame.init()
 pygame.display.set_caption('DriveRightUK')
-screenwidth=720
-screenheight=405
-screen = pygame.display.set_mode((screenwidth, screenheight),0,32)
+screenwidth = 720
+screenheight = 405
+screen = pygame.display.set_mode((screenwidth, screenheight), 0, 32)
 
-#setting font settings
+# background image
+background_image = pygame.image.load('background.jfif')
+background_image = pygame.transform.scale(background_image, (screenwidth, screenheight))
+
+#cog image
+cog_image = pygame.image.load('cog.png')
+cog_image = pygame.transform.scale(cog_image, (30,30))
+
+# Setting font settings
 font = pygame.font.SysFont(None, 30)
 
-#function that can be used to write text on screen and buttons
-def draw_text(text, font, color, surface, x, y, centered=False):
-    text_surface = font.render(text, True, color)
-    text_rect = text_surface.get_rect()
-    
-    if centered:
-        text_rect.center = (x, y)
-    else:
-        text_rect.topleft = (x, y)
-    
-    surface.blit(text_surface, text_rect)
+class Text:
+    def __init__(self, text, font, color, surface):
+        self.text = text
+        self.font = font
+        self.color = color
+        self.surface = surface
+        self.text_surface = self.font.render(self.text, True, self.color)
+        self.text_rect = self.text_surface.get_rect()
+
+    def draw(self, x, y, centered=False):
+        if centered:
+            self.text_rect.center = (x, y)
+        else:
+            self.text_rect.topleft = (x, y)
+        self.surface.blit(self.text_surface, self.text_rect)
+
+class Button:
+    def __init__(self, x, y, width, height, color, hover_color, text, font, text_color, surface, image=None):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
+        self.hover_color = hover_color
+        self.text = Text(text, font, text_color, surface)
+        self.surface = surface
+        self.image = image
+
+    def draw(self):
+        mx, my = pygame.mouse.get_pos()
+        if self.rect.collidepoint((mx, my)):
+            pygame.draw.rect(self.surface, self.hover_color, self.rect, 0, 3)
+        else:
+            pygame.draw.rect(self.surface, self.color, self.rect, 0, 3)
+        self.text.draw(self.rect.centerx, self.rect.centery, centered=True)
+
+        if self.image:
+            image_rect = self.image.get_rect(center=self.rect.center)
+            self.surface.blit(self.image, image_rect)
+
+    def is_clicked(self):
+        mx, my = pygame.mouse.get_pos()
+        if self.rect.collidepoint((mx, my)):
+            if pygame.mouse.get_pressed()[0]:
+                return True
+        return False
 
 # A variable to check for the status later
 click = False
 
 # Main container function that holds the buttons and game functions
 def main_menu():
+    global click
+    play_button = Button(10, 350, 200, 40, (255, 0, 0), (200, 0, 0), "PLAY", font, (255, 255, 255), screen)
+    options_button = Button(10, 300, 200, 40, (255, 0, 0), (200, 0, 0), "OPTIONS", font, (255, 255, 255), screen)
+    settings_button = Button(670, 80, 40, 40, (128, 128, 128), (100, 100, 100), "", font, (255, 255, 255), screen, cog_image)
+
     while True:
+        screen.blit(background_image, (0,0))
 
-        screen.fill((0,190,255))
-        draw_text("DriveRightUK", font, (0,0,0), screen, (screenwidth//2), 50, centered=True) # 
+        title_text = Text("DriveRightUK", font, (0, 0, 0), screen)
+        title_text.draw(screenwidth // 2, 50, centered=True)
 
-        mx, my = pygame.mouse.get_pos()
+        play_button.draw()
+        options_button.draw()
+        settings_button.draw()
 
-        #creating buttons
-        playButton = pygame.Rect(10, 350, 200, 40) # "play" button
-        optionsButton = pygame.Rect(10, 300, 200, 40) # "options" button
-        settingsButton = pygame.Rect(670, 80, 40, 40) # settings button
-
-        #defining functions when a certain button is pressed
-        if playButton.collidepoint((mx, my)):
-            if click:
-                game()
-        if optionsButton.collidepoint((mx, my)):
-            if click:
-                options()
-        if settingsButton.collidepoint((mx, my)):
-            if click:
-                settings()
-        pygame.draw.rect(screen, (255, 0, 0), playButton, 0, 3)
-        pygame.draw.rect(screen, (255, 0, 0), optionsButton, 0, 3)
-        pygame.draw.rect(screen, (128, 128, 128), settingsButton, 0, 100)
-
-        #writing text on top of button
-        draw_text('PLAY', font, (255,255,255), screen, 20, 360)
-        draw_text('OPTIONS', font, (255,255,255), screen, 20, 310)
-
+        if play_button.is_clicked():
+            game()
+        if options_button.is_clicked():
+            options()
+        if settings_button.is_clicked():
+            settings()
 
         click = False
         for event in pygame.event.get():
@@ -76,13 +108,15 @@ def main_menu():
         pygame.display.update()
         mainClock.tick(60)
 
-#This function is called when the "PLAY" button is clicked.
+# This function is called when the "PLAY" button is clicked.
 def game():
     running = True
     while running:
-        screen.fill((0,0,0))
+        screen.fill((0, 0, 0))
 
-        draw_text('GAME SCREEN', font, (255, 255, 255), screen, 20, 20)
+        game_text = Text('GAME SCREEN', font, (255, 255, 255), screen)
+        game_text.draw(20, 20)
+        
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -98,9 +132,11 @@ def game():
 def options():
     running = True
     while running:
-        screen.fill((0,0,0))
+        screen.fill((0, 0, 0))
 
-        draw_text('OPTIONS SCREEN', font, (255, 255, 255), screen, 20, 20)
+        options_text = Text('OPTIONS SCREEN', font, (255, 255, 255), screen)
+        options_text.draw(20, 20)
+        
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -116,9 +152,11 @@ def options():
 def settings():
     running = True
     while running:
-        screen.fill((0,190,255))
+        screen.fill((0, 190, 255))
 
-        draw_text("SETTINGS SCREEN", font, (255, 255, 255), screen, (screenwidth//2), 50, centered=True)
+        settings_text = Text("SETTINGS SCREEN", font, (255, 255, 255), screen)
+        settings_text.draw(screenwidth // 2, 50, centered=True)
+        
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
